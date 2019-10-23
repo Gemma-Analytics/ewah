55166ff90d795a3196264d4c220fbd0fd9d207b3
Bijan Soltani <me@bijansoltani.com>
2019-10-23 21:39:08 +0200

    dbt DAG function

--- ewah/ewah_operators/ewah_postgres_operator.py
+++ ewah/ewah_operators/ewah_postgres_operator.py
@@ -8,14 +8,13 @@ class EWAHPostgresOperator(EWAHSQLBaseOperator):
     _SQL_BASE_ALL = 'SELECT * FROM "{0}"."{1}"\nWHERE {{0}};'
     _SQL_COLUMN_QUOTE = '"'
     _SQL_MINMAX_CHUNKS = 'SELECT MIN("{0}"), MAX("{0}") FROM "{1}"."{2}";'
-    _SQL_CHUNKING_CLAUSE = '"{0}" >= %(from)s AND "{0}" <{1} %(until)s'
+    _SQL_CHUNKING_CLAUSE = 'AND "{0}" >= %(from)s AND "{0}" <{1} %(until)s'
 
     def __init__(self, *args, **kwargs):
         self.sql_engine = self._PGSQL
         super().__init__(*args, **kwargs)
 
     def _get_data_from_sql(self, sql, params=None, return_dict=True):
-        self.log.info('Executing:\n{0}'.format(sql))
         hook = EWAHDWHookPostgres(self.source_conn_id)
         data = hook.execute_and_return_result(
             sql=sql,
