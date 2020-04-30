c8b59117e22f0abbf6be55f96158348fbb4fc359
Bijan Soltani <me+github@bijansoltani.com>
2020-04-30 19:40:06 +0200

    make pypi ready no 2

--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
+import setuptools
 
-# https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-from distutils.core import setup
-setup(
-  name = 'ewah',
-  packages = ['ewah'],
-  version = '0.1.4',
-  license='MIT',
-  description = 'An ELT with airflow helper module: Ewah',
-  author = 'Bijan Soltani',
-  author_email = 'bijan.soltani+ewah@gemmaanalytics.com',
-  url = 'https://gemmaanalytics.com/',
-  download_url = 'https://github.com/Gemma-Analytics/ewah/archive/0.1.4.tar.gz',
-  keywords = ['airflow', 'ELT', 'ETL'],
-  install_requires=[
-          'pyyaml',
-          'psycopg2',
-          'gspread',
-          'pytz',
-          'yahoofinancials',
-          'apiclient',
-          'oauth2client',
-          'cx_Oracle',
-          'facebook_business',
-          'mysql',
-          'snowflake',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-  ],
+setuptools.setup(
+    name="bijansoltani", # Replace with your own username
+    version="0.1.4",
+    author="Bijan Soltani",
+    author_email="bijan.soltani+ewah@gemmaanalytics.com",
+    description="An ELT with airflow helper module: Ewah",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gemmaanalytics.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
 )
