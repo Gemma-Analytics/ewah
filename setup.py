5557bd056a92e92ea1f573e0f1f528580ce2712a
Bijan Soltani <me+github@bijansoltani.com>
2020-04-30 21:38:57 +0200

    fixed install requirements, bumped to 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ewah", # Replace with your own username
-    version="0.1.7",
+    version="0.1.8",
     author="Bijan Soltani",
     author_email="bijan.soltani+ewah@gemmaanalytics.com",
     description="An ELT with airflow helper module: Ewah",
@@ -18,4 +18,17 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
+    install_requires=[            # I get to this in a second
+          'pyyaml',
+          'psycopg2',
+          'gspread',
+          'pytz',
+          'yahoofinancials',
+          'google-api-python-client',
+          'oauth2client',
+          'cx_Oracle',
+          'facebook_business',
+          'mysql-connector-python',
+          'snowflake-connector-python',
+      ],
 )
