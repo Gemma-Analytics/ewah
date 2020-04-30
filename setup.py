8a18f12c69738c3b6644240c35d84c6266d88591
Bijan Soltani <me+github@bijansoltani.com>
2020-04-30 19:04:29 +0200

    make pypi ready

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
-from setuptools import setup, find_packages
 
+# https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+
+from distutils.core import setup
 setup(
-    name='ewah',
-    version='0.1.3',
-    description='An ELT with airflow helper module: Ewah',
-    url='https://github.com/soltanianalytics/ewah',
-    author='Bijan Soltani',
-    author_email='me+ewah@bijansoltani.com',
-    license='MIT',
-    packages=['ewah'],
-    zip_safe=False,
-    python_requires=">=3.7",
+  name = 'ewah',
+  packages = ['ewah'],
+  version = '0.1.4',
+  license='MIT',
+  description = 'An ELT with airflow helper module: Ewah',
+  author = 'Bijan Soltani',
+  author_email = 'bijan.soltani+ewah@gemmaanalytics.com',
+  url = 'https://gemmaanalytics.com/',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['airflow', 'ELT', 'ETL'],
+  install_requires=[            # I get to this in a second
+          'pyyaml',
+          'psycopg2',
+          'gspread',
+          'pytz',
+          'yahoofinancials',
+          'apiclient',
+          'oauth2client',
+          'cx_Oracle',
+          'facebook_business',
+          'mysql',
+          'snowflake',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
 )
