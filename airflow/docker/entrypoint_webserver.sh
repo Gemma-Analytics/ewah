627bef49d01e5746cc324ab963bacc27cb2d26d1
Bijan Soltani <me+github@bijansoltani.com>
2020-11-21 21:40:34 +0100

    add two scripts to airflow startup routine

--- airflow/docker/entrypoint_webserver.sh
+++ airflow/docker/entrypoint_webserver.sh
@@ -2,5 +2,7 @@
 
 airflow upgradedb && \
   pip install --user --upgrade -e /opt/ewah && \
+  python /opt/airflow/docker/remove_extra_length_constraint.py && \
   python /opt/airflow/docker/add_conns.py && \
+  python /opt/airflow/docker/add_admin.py && \
   airflow webserver
