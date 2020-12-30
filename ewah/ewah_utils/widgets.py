c9951acdeeaffe2894dac5fe3a58d1135fe186ff
Bijan <me+git@bijansoltani.com>
2020-12-30 19:52:20 +0100

    improve widgets

--- ewah/ewah_utils/widgets.py
+++ ewah/ewah_utils/widgets.py
@@ -4,9 +4,13 @@ from wtforms import widgets
 class EWAHTextAreaWidget(widgets.TextArea):
     """Make a variable-depth Text Area widget."""
 
+    def __init__(self, rows=3):
+        self.rows = rows
+        return super().__init__()
+
     def __call__(self, field, **kwargs):
         kwargs["class"] = u"form-control"
-        kwargs["rows"] = kwargs.get("rows", 3)
+        kwargs["rows"] = kwargs.get("rows", self.rows)
         if field.label:
             kwargs["placeholder"] = field.label.text
         return super().__call__(field, **kwargs)
