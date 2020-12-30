ffc2790275554f1d16a259e21c54d08bb2d83286
Bijan <me+git@bijansoltani.com>
2020-12-30 15:14:54 +0100

    refactor ssh tunnel module content

--- ewah/ewah_utils/ssh_tunnel.py
+++ ewah/ewah_utils/ssh_tunnel.py
@@ -1,27 +1,6 @@
-"""
-    A utility to use SSH tunnel port forwarding including SSH agent forwarding.
-    The motivation is that existing packages either do SSH tunnel forwarding
-    well (`sshtnnel.SSHTunnelForwarder`) or they do SSH agent forwarding
-    (`paramiko`), but no out of the box solution can provide both. EWAH
-    uses this utility to implement SSH tunneling with and without SSH agent
-    forwarding across all operators.
+from ewah.hooks.base import EWAHBaseHook
 
-    Inspiration for the utility came from:
-    - This stack overflow post: https://stackoverflow.com/questions/23666600/ssh-key-forwarding-using-python-paramiko
-    - This implementation of SSH port forwarding: https://github.com/paramiko/paramiko/blob/master/demos/forward.py
-    - This implementation of SSH agent forwarding: https://gist.github.com/toejough/436540622530c35404e6
-"""
-
-from airflow.hooks.base_hook import BaseHook
-
-import getpass
 import os
-import socket
-import select
-import socketserver
-import sys
-import paramiko
-import threading
 
 from tempfile import NamedTemporaryFile
 
@@ -42,9 +21,11 @@ def start_ssh_tunnel(ssh_conn_id, remote_conn_id, custom_timeout=None):
     the open tunnel)
     """
     if custom_timeout:
+        previous_timeout = sshtunnel.TUNNEL_TIMEOUT
         sshtunnel.TUNNEL_TIMEOUT = custom_timeout
-    ssh_conn = BaseHook.get_connection(ssh_conn_id)
-    remote_conn = BaseHook.get_connection(remote_conn_id)
+
+    ssh_conn = EWAHBaseHook.get_connection(ssh_conn_id)
+    remote_conn = EWAHBaseHook.get_connection(remote_conn_id)
     kwargs = {
         'ssh_address_or_host': (ssh_conn.host, ssh_conn.port or 22),
         'remote_bind_address': (remote_conn.host, remote_conn.port),
@@ -56,8 +37,12 @@ def start_ssh_tunnel(ssh_conn_id, remote_conn_id, custom_timeout=None):
 
     with NamedTemporaryFile() as keyfile:
         extra = ssh_conn.extra
+        extra_dejson = ssh_conn.extra_dejson
         if extra:
-            keyfile.write(extra.encode())
+            if extra_dejson:
+                keyfile.write(extra_dejson.get('extra__private_key', '').encode())
+            else:
+                keyfile.write(extra.encode())
             keyfile.flush()
             kwargs['ssh_pkey'] = os.path.abspath(keyfile.name)
         ssh_tunnel_forwarder = sshtunnel.SSHTunnelForwarder(**kwargs)
@@ -65,5 +50,8 @@ def start_ssh_tunnel(ssh_conn_id, remote_conn_id, custom_timeout=None):
 
     remote_conn.host = 'localhost'
     remote_conn.port = ssh_tunnel_forwarder.local_bind_port
+
+    if custom_timeout:
+        sshtunnel.TUNNEL_TIMEOUT = previous_timeout
 
     return ssh_tunnel_forwarder, remote_conn
