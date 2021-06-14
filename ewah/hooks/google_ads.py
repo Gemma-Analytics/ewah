51791f577dc1a5d58e286deda661976289d435d3
Bijan <me+enpal@bijansoltani.com>
2021-06-14 19:10:20 +0200

    upgrade google ads api version

---
+++ ewah/hooks/google_ads.py
@@ -0,0 +1,123 @@
+from ewah.constants import EWAHConstants as EC
+from ewah.hooks.base import EWAHBaseHook
+
+from google.ads.google_ads.client import GoogleAdsClient
+from google.protobuf.json_format import MessageToDict
+
+from datetime import datetime
+
+
+class EWAHGoogleAdsHook(EWAHBaseHook):
+
+    _ATTR_RELABEL: {
+        "client_id": "login",
+        "client_secret": "password",
+    }
+
+    conn_name_attr = "ewah_google_ads_conn_id"
+    default_conn_name = "ewah_google_ads_default"
+    conn_type = "ewah_google_ads"
+    hook_name = "EWAH Google Ads Connection"
+
+    @staticmethod
+    def get_ui_field_behaviour() -> dict:
+        return {
+            "hidden_fields": ["extra", "schema", "host", "port"],
+            "relabeling": {
+                "login": "client_id",
+                "password": "client_secret",
+            },
+        }
+
+    @staticmethod
+    def get_connection_form_widgets() -> dict:
+        """Returns connection widgets to add to connection form"""
+        # from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+        from ewah.ewah_utils.widgets import EWAHTextAreaWidget
+        from wtforms import PasswordField
+
+        return {
+            "extra__ewah_google_ads__developer_token": PasswordField("Developer Token"),
+            "extra__ewah_google_ads__refresh_token": PasswordField("Refresh Token"),
+        }
+
+    @property
+    def service(self):
+        if not hasattr(self, "_service"):
+            self._service = GoogleAdsClient.load_from_dict(
+                {
+                    "developer_token": self.conn.developer_token,
+                    "client_id": self.conn.login,
+                    "client_secret": self.conn.password,
+                    "refresh_token": self.conn.refresh_token,
+                }
+            ).get_service("GoogleAdsService")
+        return self._service
+
+    @staticmethod
+    def create_query(fields, resource, conditions=None):
+        def format_columns(dict_to_format, prefix=None):
+            # create the list of fields for the SELECT statement
+            if prefix is None:
+                prefix = ""
+            elif not prefix[-1] == ".":
+                prefix += "."
+            fields = []
+            for key, value in dict_to_format.items():
+                for item in value:
+                    if type(item) == dict:
+                        fields += format_columns(item, prefix + key)
+                    else:
+                        fields += [prefix + key + "." + item]
+            return fields
+
+        query = "SELECT {0}\nFROM {1}".format(
+            ", ".join(format_columns(fields)), resource
+        )
+        if conditions:
+            query += "\nWHERE {0}".format("\n\tAND ".join(conditions))
+        return query
+
+    def transform_raw_data_to_relational_format(self, raw_row, _prefix=None):
+        """Each row of the returned data is a protobuf message that can have many
+        layers. Unpack it into a 1-layer dictionary."""
+        final_dict = {}
+        if _prefix:
+            prefix = _prefix + "__"
+        else:
+            prefix = ""
+            raw_row = MessageToDict(raw_row, preserving_proto_field_name=True)
+        for key, value in raw_row.items():
+            if isinstance(value, dict):
+                final_dict.update(
+                    self.transform_raw_data_to_relational_format(
+                        value, _prefix=prefix + key
+                    )
+                )
+            else:
+                final_dict[prefix + key] = value
+        if not _prefix and final_dict.get("segments__date"):
+            final_dict["segments__date"] = datetime.strptime(
+                final_dict["segments__date"], "%Y-%m-%d"
+            ).date()
+        return final_dict
+
+    def get_raw_data_from_query(self, client_id, query):
+        self.log.info("Running query:\n\n{0}\n\n".format(query))
+        return [
+            row
+            for row in self.service.search(
+                customer_id=client_id.replace("-", ""), query=query
+            )
+        ]
+
+    def get_data(self, client_id, fields, resource, conditions=None):
+        return [
+            self.transform_raw_data_to_relational_format(raw_row=row)
+            for row in self.get_raw_data_from_query(
+                client_id=client_id,
+                query=self.create_query(
+                    fields=fields, resource=resource, conditions=conditions
+                ),
+            )
+        ]
--- ewah/operators/google_ads.py
+++ ewah/operators/google_ads.py
@@ -2,11 +2,9 @@ from ewah.operators.base import EWAHBaseOperator
 from ewah.ewah_utils.python_utils import is_iterable_not_string
 from ewah.constants import EWAHConstants as EC
 
-from google.ads.google_ads.client import GoogleAdsClient
-from ewah.hooks.base import EWAHBaseHook as BaseHook
+from ewah.hooks.google_ads import EWAHGoogleAdsHook
 
 from datetime import datetime, timedelta
-from copy import deepcopy
 
 
 class EWAHGoogleAdsOperator(EWAHBaseOperator):
@@ -14,24 +12,19 @@ class EWAHGoogleAdsOperator(EWAHBaseOperator):
     _NAMES = ["gads", "google_ads"]
 
     _ACCEPTED_EXTRACT_STRATEGIES = {
-        EC.ES_FULL_REFRESH: False,
+        EC.ES_FULL_REFRESH: True,
         EC.ES_INCREMENTAL: True,
+        EC.ES_SUBSEQUENT: True,
     }
 
-    _REQUIRED_KEYS = (
-        # You must have a developer_token to use the Google Ads API!
-        "developer_token",
-        "client_id",
-        "client_secret",
-        "refresh_token",
-    )
+    _CONN_TYPE = EWAHGoogleAdsHook.conn_type
 
     def __init__(
         self,
-        fields,  # dict of fields, e.g. {'campaign': ['id', 'name']}
-        metrics,  # list of metrics to retrieve -
-        resource,  # name of the resource, e.g. ad_group
         client_id,
+        resource,  # name of the resource, e.g. ad_group
+        fields,  # dict of fields, e.g. {'campaign': ['id', 'name']}
+        metrics=None,  # list of metrics to retrieve - deprecated!
         conditions=None,  # list of conditions, e.g. ["ad_group.status = 'ENABLED'"]
         *args,
         **kwargs
@@ -42,33 +35,38 @@ class EWAHGoogleAdsOperator(EWAHBaseOperator):
         """
 
         if kwargs.get("columns_definition"):
-            raise Exception(
-                "columns_definition is not accepted for this " + "operator!"
-            )
+            raise Exception("columns_definition is not accepted for this operator!")
 
-        kwargs["update_on_columns"] = [
-            col.replace(".", "_")
-            for col in self.get_select_statement(fields)
-            if col[:7] == "segment"
-        ] + ["{0}_resource_name".format(resource)]
+        if metrics:
+            fields["metrics"] = metrics
+
+        kwargs["update_on_columns"] = list(
+            set(
+                [
+                    "segments__" + col.replace(".", "__")
+                    for col in fields.get("segments", [])
+                ]
+                + ["{0}__resource_name".format(resource)]
+            )
+        )
 
         if conditions and not is_iterable_not_string(conditions):
             raise Exception('Argument "conditions" must be a list!')
 
-        if not (fields.get("segments") and ("date" in fields["segments"])):
-            raise Exception("fields list MUST contain segments.date!")
-
-        # must use deepcopy due to airflow calling field by reference
-        # and the update in the following row adding the metrics to the field,
-        # which in turn screws with the update_on_columns definition above
-        self.fields_dict = deepcopy(fields)
-        self.fields_dict.update({"metrics": deepcopy(metrics)})
-        self.fields_list = self.get_select_statement(self.fields_dict)
+        self.fields = fields
         self.resource = resource
         self.client_id = str(client_id)
         self.conditions = conditions
 
+        if kwargs.get("extract_strategy") == EC.ES_SUBSEQUENT:
+            kwargs["subsequent_field"] = "segments__date"
+
         super().__init__(*args, **kwargs)
+
+        if not self.extract_strategy == EC.ES_FULL_REFRESH:
+            assert "date" in fields.get(
+                "segments", []
+            ), "If using non-full-refresh loading, must include segments.date!"
 
     def get_select_statement(self, dict_format, prefix=None):
         # create the list of fields for the SELECT statement
@@ -86,88 +84,31 @@ class EWAHGoogleAdsOperator(EWAHBaseOperator):
         return fields
 
     def ewah_execute(self, context):
-        # Task execution happens here
-        def get_data_from_ads_output(fields_dict, values, prefix=None):
-            if prefix is None:
-                prefix = ""
-            elif not prefix[-1] == "_":
-                prefix += "_"
-                # e.g. 2b prefix = 'ad_group_criterion_'
-            data = {}
-            for key, value in fields_dict.items():
-                # e.g. 1 key = 'metrics', value = ['impressions', 'clicks']
-                # e.g. 2a key = 'ad_group_criterion', value = [{'keyword': ['text', 'match_type']}]
-                # e.g. 2b key = 'keyword', value = ['text', 'match_type']
-                node = getattr(values, key)
-                # e.g. 1 node = row.metrics
-                # e.g. 2a node = row.ad_group_criterion
-                # e.g. 2b node = row.ad_group_criterion.keyword
-                for item in value:
-                    # e.g. 1 item = 'clicks'
-                    # e.g. 2a item = {'keyword': ['text', 'match_type']}
-                    # e.g. 2b item = 'text'
-                    if type(item) == dict:
-                        data.update(
-                            get_data_from_ads_output(
-                                fields_dict=item,
-                                values=node,
-                                prefix=prefix
-                                + key,  # e.g. 2a '' + 'ad_group_criterion'
-                            )
-                        )
-                    else:
-                        # e.g. 1: {'' + 'metrics' + '_' + 'clicks': row.metrics.clicks.value}
-                        # e.g. 2b: {'ad_group_criterion_' + 'keyeword' + '_' + 'text': row.ad_group_criterion.keyword.text.value}
-                        if hasattr(getattr(node, item), "value"):
-                            data.update(
-                                {prefix + key + "_" + item: getattr(node, item).value}
-                            )
-                        else:
-                            # some node ends don't respond to .value but are
-                            #   already the value
-                            data.update(
-                                {prefix + key + "_" + item: getattr(node, item)}
-                            )
-            return data
-
-        conn = self.source_conn.extra_dejson
-        credentials = {}
-        for key in self._REQUIRED_KEYS:
-            if not key in conn.keys():
-                raise Exception("{0} must be in connection extra json!".format(key))
-            credentials[key] = conn[key]
-
-        # build the query
-        query = "SELECT {0} FROM {1} WHERE segments.date {2} {3}".format(
-            ", ".join(self.fields_list),
-            self.resource,
-            "BETWEEN '{0}' AND '{1}'".format(
-                self.data_from.strftime("%Y-%m-%d"),
-                self.data_until.strftime("%Y-%m-%d"),
-            ),
-            ("AND" + " AND ".join(self.conditions)) if self.conditions else "",
-        )
-
-        self.log.info("executing this google ads query:\n{0}".format(query))
-        cli = GoogleAdsClient.load_from_dict(credentials)
-        service = cli.get_service("GoogleAdsService")
-        search = service.search(
-            self.client_id.replace("-", ""),
-            query=query,
-        )
-        data = [row for row in search]
-
-        # get into uploadable format
-        upload_data = []
-        fields_dict = deepcopy(self.fields_dict)
-        fields_dict.update({self.resource: ["resource_name"]})
-        while data:
-            datum = data.pop(0)
-            upload_data += [
-                get_data_from_ads_output(
-                    fields_dict,
-                    datum,
+        # Make sure the correct date conditions are set
+        conditions = self.conditions or []
+        if (
+            self.extract_strategy == EC.ES_SUBSEQUENT
+            and self.test_if_target_table_exists()
+        ):
+            data_from = self.get_max_value_of_column(self.subsequent_field) - (
+                self.load_data_from_relative or timedelta(days=0)
+            )
+            data_until = (self.data_until or datetime.now()).date()
+        else:
+            data_from = self.data_from.date()
+            data_until = (self.data_until or datetime.now()).date()
+        if data_from and data_until:
+            conditions.append(
+                "segments.date BETWEEN '{0}' AND '{1}'".format(
+                    data_from.isoformat(),
+                    data_until.isoformat(),
                 )
-            ]
-
-        self.upload_data(upload_data)
+            )
+        self.upload_data(
+            self.source_hook.get_data(
+                client_id=self.client_id,
+                fields=self.fields,
+                resource=self.resource,
+                conditions=conditions,
+            )
+        )
