d687e6e0d6e85307c0bd8654ca5bdee6a20f01b4
Bijan <me+enpal@bijansoltani.com>
2022-01-14 19:53:14 +0100

    add rapidmail operator

---
+++ ewah/operators/rapidmail.py
@@ -0,0 +1,24 @@
+from ewah.constants import EWAHConstants as EC
+from ewah.hooks.rapidmail import EWAHRapidmailHook
+from ewah.operators.base import EWAHBaseOperator
+
+
+class EWAHGoogleAdsOperator(EWAHBaseOperator):
+
+    _NAMES = ["rapidmail"]
+
+    _ACCEPTED_EXTRACT_STRATEGIES = {
+        EC.ES_FULL_REFRESH: True,
+        EC.ES_INCREMENTAL: False,
+        EC.ES_SUBSEQUENT: False,
+    }
+
+    _CONN_TYPE = EWAHRapidmailHook.conn_type
+
+    def __init__(self, endpoint=None, *args, **kwargs):
+        self.endpoint = endpoint or kwargs.get("target_table_name")
+        super().__init__(*args, **kwargs)
+
+    def ewah_execute(self, context):
+        for batch in self.source_hook.get_all_data(endpoint=self.endpoint):
+            self.upload_data(batch)
