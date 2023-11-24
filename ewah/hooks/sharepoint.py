f6fc1dae6871cdb05ba492ff2b65e0ddc922ff7c
Bijan Soltani <bijan.soltani@gemmaanalytics.com>
2023-11-24 17:58:56 +0100

    batching sharepoint

--- ewah/hooks/sharepoint.py
+++ ewah/hooks/sharepoint.py
@@ -10,7 +10,6 @@ import io
 
 
 class EWAHSharepointHook(EWAHBaseHook):
-
     _ATTR_RELABEL: dict = {
         "user": "login",
         "site_url": "schema",
@@ -32,9 +31,27 @@ class EWAHSharepointHook(EWAHBaseHook):
             },
         }
 
-    def get_data_from_excel(self, relative_url, worksheet_name, header_row, start_row):
+    def get_data_from_excel(
+        self,
+        relative_url: str,
+        worksheet_name: str,
+        header_row: int,
+        start_row: int,
+        batch_size: int,
+    ):
         # adapted from: https://stackoverflow.com/a/69292234/14125255
         # (accessed 2021-10-15)
+
+        assert (
+            isinstance(batch_size, int) and batch_size > 0
+        ), f"type: {type(batch_size)}, value: {str(batch_size)}"
+        assert (
+            isinstance(header_row, int) and header_row > 0
+        ), f"type: {type(header_row)}, value: {str(header_row)}"
+        assert (
+            isinstance(start_row, int) and start_row > 0
+        ), f"type: {type(start_row)}, value: {str(start_row)}"
+        assert start_row > header_row
 
         ctx = ClientContext(self.conn.site_url).with_credentials(
             UserCredential(self.conn.user, self.conn.password)
@@ -49,8 +66,9 @@ class EWAHSharepointHook(EWAHBaseHook):
             for col in range(1, ws.max_column + 1)
             if ws.cell(row=header_row, column=col)
         }
-        data = [
-            {k: ws.cell(row=row, column=v).value for k, v in headers.items()}
-            for row in range(start_row, ws.max_row + 1)
-        ]
-        return data
+        while start_row < ws.max_row + 1:
+            yield [
+                {k: ws.cell(row=row, column=v).value for k, v in headers.items()}
+                for row in range(start_row, min(start_row + batch_size, ws.max_row + 1))
+            ]
+            start_row += batch_size
