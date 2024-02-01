7454b22b4a46063dda7fc6d1a573721d7920bc2c
Dimitriy Ni <dimitriy.ni@outlook.com>
2024-02-01 15:56:51 +0100

    update googleads api version

--- ewah/hooks/google_ads.py
+++ ewah/hooks/google_ads.py
@@ -64,7 +64,7 @@ class EWAHGoogleAdsHook(EWAHBaseHook):
                 config_dict["login_customer_id"] = self.conn.schema.replace("-", "")
             self._service = GoogleAdsClient.load_from_dict(
                 config_dict=config_dict,
-                version="v13",
+                version="v18",
             ).get_service("GoogleAdsService")
 
         return self._service
